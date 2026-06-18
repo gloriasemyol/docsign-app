5aea021202d5fcce2fd1a235431e34632f65ed3d
Gloria Semyol <gloriasemyol@gmail.com>
2026-06-18 14:23:36 +0530

    Fix full react-pdf path build error

--- frontend/src/pages/PDFEditor.jsx
+++ frontend/src/pages/PDFEditor.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 import axios from 'axios';
 
 // Required setup for react-pdf
